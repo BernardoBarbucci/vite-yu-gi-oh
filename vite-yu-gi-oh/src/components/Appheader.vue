093235ef1a00a8a8f37a38ba88666b88e6ad7eed
“Bernardo <bernardobarbucci@gmail.com>
2023-12-19 18:59:52 +0100

    header completato

--- vite-yu-gi-oh/src/components/Appheader.vue
+++ vite-yu-gi-oh/src/components/Appheader.vue
@@ -1,19 +1,30 @@
 <template>
   <header>
-    <nav class="navbar bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand fw-bold">Yu-gi-oh API</a>
-        <form class="d-flex" role="search">
+    <nav class="navbar bg-light">
+      <div class="container-fluid bg-light">
+        <a class="navbar-brand fs-3 bg-light">
+          <img src="../components/img/Yugioh_anime_logo.webp" class="me-4" alt="Logo" width="120">
+          {{ title }}
+        </a>
+        <!-- <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        </form> -->
       </div>
     </nav>
   </header>
 </template>
+
 <script>
 export default {
   name: 'Appheader',
+
+  data() {
+    return {
+      title: 'Yu-gi-oh API'
+    }
+  }
 }
 </script>
+
 <style lang="scss" scoped></style>
